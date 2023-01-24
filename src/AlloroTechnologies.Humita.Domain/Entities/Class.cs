478e19b852eea3c2cf8f0ca02130a053a50b3eb0
Alexis Henríquez <alexis.henriquezb@gmail.com>
2023-01-23 22:31:59 -0300

    Initialize BaseClass property of Class.

--- src/AlloroTechnologies.Humita.Domain/Entities/Class.cs
+++ src/AlloroTechnologies.Humita.Domain/Entities/Class.cs
@@ -3,7 +3,7 @@ namespace AlloroTechnologies.Humita.Domain.Entities;
 public class Class : IValidatable
 {
     public List<Interface> ImplementedInterfaces { get; set; } = new List<Interface>();
-    public Class BaseClass { get; set; } = default!;
+    public Class BaseClass { get; set; } = new Class();
     public List<Property> Properties { get; set; } = new List<Property>();
     public List<Operation> Operations { get; set; } = new List<Operation>();
     public AccessModifier AccessModifier { get; set; } = AccessModifier.Public;
